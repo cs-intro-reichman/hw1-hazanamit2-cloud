a13212bf37fe5faa41d6afd58334d0ccbe51ed9a
n5snszyzc9-svg <amit.hazan01@post.runi.ac.il>
2025-11-06 12:18:26 +0200

    first try

--- PrintTo5.java
+++ PrintTo5.java
@@ -2,7 +2,7 @@
 // (Same program as shown in lecture 1-1).
 public class PrintTo5 {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
+		// Declares an int variable, and sets it to 1 this is not working
         int i = 0;
         // Complete the program's code here:
     }
