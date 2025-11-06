16b43754e97dcc899584639b4521896861996bd1
n5snszyzc9-svg <amit.hazan01@post.runi.ac.il>
2025-11-06 19:33:27 +0200

    fix num

--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,7 @@
 public class NumWords {
 	public static void main(String args[]) {
 		int num = Integer.parseInt ( args [0] );
-		System.out.println ( (num / 100) + " hundreds " + ((num / 10) % 10)  + " tens " + (num % 10) + " ones " );
+		System.out.println ( (num / 100) + " hundreds, " + ((num / 10) % 10)  + " tens, and " + (num % 10) + " ones " );
 	    // Replace this comment with your code
 	}
 }
